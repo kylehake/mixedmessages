b5c8bab0011a70c785cab576634e3ddb454b0375
Kyle Hake <kylehake@Kyles-MacBook-Pro.local>
2021-02-19 03:20:14 -0600

    Created variables for generator function

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const beginningPhrases = [];
+const beginningPhrases = ['A bird in', ];
 
-const middlePhrases = [];
+const middlePhrases = ['the hand is worth', ];
 
-const endPhrases = [];
+const endPhrases = ['two in the bush', ];
 
 
 const chineseFortuneGenerator = () => {
-
+    let beginning = beginningPhrases[Math.floor(Math.random()*(beginningPhrases.length))];
+    let middle = middlePhrases[Math.floor(Math.random()*(middlePhrases.length))];
+    let end = endPhrases[Math.floor(Math.random()*(endPhrases.length))];
 }; 
